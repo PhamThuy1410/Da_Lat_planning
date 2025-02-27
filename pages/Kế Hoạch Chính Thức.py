957467f4d667aa0154c1fa23c7a8999f75f1abe7
PhamThuy1410 <110886311+PhamThuy1410@users.noreply.github.com>
2025-02-27 15:13:35 +0700

    Update Kế Hoạch Chính Thức.py

--- pages/Kế Hoạch Chính Thức.py
+++ pages/Kế Hoạch Chính Thức.py
@@ -33,8 +33,8 @@ st.markdown(
     """
     <style>
         body { background-color: #0E1117; color: white; }
-        h1 { color: #27667B !important; font-size: 50px !important; font-weight: 700 !important; }
-        h2 { color: #A4B465 !important; font-size: 40px !important; font-weight: 700 !important; }
+        h1 { color: #DDEB9D !important; font-size: 50px !important; font-weight: 700 !important; }
+        h2 { color: #27667B !important; font-size: 40px !important; font-weight: 700 !important; }
         .custom-metric-label { color: #FFCF50 !important; font-weight: 700 !important; font-size: 25px !important; }
         .custom-metric-value { color: #FFFFFF !important; font-size: 30px !important; }
     </style>
