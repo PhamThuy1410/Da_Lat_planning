1a23743e20f5639816d2f542fb91537f3d0d8f01
PhamThuy1410 <110886311+PhamThuy1410@users.noreply.github.com>
2025-03-23 22:47:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ st.write("Vui lòng chọn trang:")
 
 # Hiển thị liên kết đến các trang
 st.page_link("pages/Chat bot.py", label="Chat bot", icon="🤖")
+st.page_link("pages/Lưu ý và ghi nhớ.py", label="Lưu Ý Và Ghi Nhớ", icon="🎈")
 st.page_link("pages/Kế Hoạch Chính Thức.py", label="Kế Hoạch Chính Thức", icon="📖")
 st.page_link("pages/Gợi ý.py", label="Gợi Ý", icon="💡")
+st.page_link("pages/Weather.py", label="Thời tiết", icon="☁️")
