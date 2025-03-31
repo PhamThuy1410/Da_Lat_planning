5610c9350aac8a038df723a7808a300e4cb7d164
PhamThuy1410 <110886311+PhamThuy1410@users.noreply.github.com>
2025-03-31 10:12:57 +0700

    Update Kế Hoạch Chính Thức.py

--- pages/Kế Hoạch Chính Thức.py
+++ pages/Kế Hoạch Chính Thức.py
@@ -33,10 +33,10 @@ st.markdown(
     """
     <style>
         body { background-color: #0E1117; color: white; }
-        h1 { color: #A0D683 !important; font-size: 50px !important; font-weight: 700 !important; font-style: Anton}
-        h2 { color: #B3C8CF !important; font-size: 40px !important; font-weight: 700 !important; font-style: Anton}
-        .custom-metric-label { color: #FFE3E3 !important; font-weight: 700 !important; font-size: 25px !important; }
-        .custom-metric-value { color: #FFFFFF !important; font-size: 30px !important; }
+        h1 { color: #A0D683 !important; font-size: 50px !important; font-weight: 700 !important; font-style: Anton; }
+        h2 { color: #B3C8CF !important; font-size: 40px !important; font-weight: 700 !important; font-style: Anton; }
+        .custom-metric-label { color: #FFE3E3 !important; font-weight: 700 !important; font-size: 25px !important; font-style: Anton; }
+        .custom-metric-value { color: #FFFFFF !important; font-size: 30px !important;font-style: Anton; }
     </style>
     """,
     unsafe_allow_html=True
