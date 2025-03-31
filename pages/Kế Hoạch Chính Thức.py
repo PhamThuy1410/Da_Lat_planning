28524e44f0d4ce07b3ca2a88eb5f108f4a78f4a1
PhamThuy1410 <110886311+PhamThuy1410@users.noreply.github.com>
2025-03-31 10:11:02 +0700

    Update Kế Hoạch Chính Thức.py

--- pages/Kế Hoạch Chính Thức.py
+++ pages/Kế Hoạch Chính Thức.py
@@ -29,18 +29,18 @@ def save_data(sheet_name, df):
         st.error(f"Lỗi khi lưu dữ liệu: {e}")
 
 # Thiết lập giao diện Dark Mode và CSS tùy chỉnh
-st.markdown(
-    """
-    <style>
-        body { background-color: #0E1117; color: white; }
-        h1 { color: #A0D683 !important; font-size: 50px !important; font-weight: 700 !important; }
-        h2 { color: #B3C8CF !important; font-size: 40px !important; font-weight: 700 !important; }
-        .custom-metric-label { color: #FFE3E3 !important; font-weight: 700 !important; font-size: 25px !important; }
-        .custom-metric-value { color: #FFFFFF !important; font-size: 30px !important; }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+    body, h1, h2, .custom-metric-label, .custom-metric-value {
+        font-family: 'Anton', sans-serif;
+        color: white;
+    }
+    body { background-color: #0E1117; }
+    h1 { color: #A0D683; font-size: 50px; font-weight: 700; }
+    h2 { color: #B3C8CF; font-size: 40px; font-weight: 700; }
+    .custom-metric-label { color: #FFE3E3; font-size: 25px; font-weight: 700; }
+    .custom-metric-value { font-size: 30px; }
+</style>
 
 st.title("PROJECT: ĐÀ LẠT PLANNING")
 
